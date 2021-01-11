8227ec0533c0f662f76d2d3b7927c22996d0f71e
Anna <t-avoitika@hotels.com>
2021-01-11 22:16:57 +0000

    Terminal shows a random T Swift song

---
+++ getsongs.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+import os
+import requests
+import bs4
+import pandas as pd
+
+PATH = os.path.join("/", "Users", "avoitika", "Projects", "TaylorSwiftie")
+
+
+def table_to_df(table):
+           return pd.DataFrame([[th.text for th in row.findAll('th')] for row in table.tbody.findAll('tr')])
+
+res = pd.DataFrame()
+url = 'https://en.wikipedia.org/wiki/List_of_songs_recorded_by_Taylor_Swift'
+
+page = requests.get(url)
+soup = bs4.BeautifulSoup(page.content, 'lxml')
+table = soup.find(name='table', attrs={'class':'plainrowheaders'})
+res = res.append(table_to_df(table))
+res.to_csv(os.path.join(os.path.join(PATH, "table.csv")),
+           index=None, sep=';', encoding='cp1252')
